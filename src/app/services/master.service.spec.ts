336076bbc0eb051ab4659fd26496c0be0c8e6e39
Alberto Monterrubio <amonterrubio@masteredi.com.mx>
2023-04-21 09:19:29 -0600

    un cambio

--- src/app/services/master.service.spec.ts
+++ src/app/services/master.service.spec.ts
@@ -6,8 +6,13 @@ describe('MasterService', () => {
   
   it('should be return "My value" fron real service', () => {
     let valueService = new ValueService();
-    let masterService = new MasterService(valueService);    
+    let masterService = new MasterService(valueService); 
+    expect(masterService.getValue()).toBe('my value');
+  });
 
+  it('should be return "other value" fron the fake service', () => {
+    let valueService = new ValueService();
+    let masterService = new MasterService(valueService); 
     expect(masterService.getValue()).toBe('my value');
   });
 });
